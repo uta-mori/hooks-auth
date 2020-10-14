5e0fe8c92490a3d9edd15e5f140e6e63540936eb
uta-mori <mori_yuta@retail-ai.jp>
2020-10-14 15:14:32 +0900

    ✨ ユーザーをDBに登録

--- hooks/authentication.ts
+++ hooks/authentication.ts
@@ -8,6 +8,17 @@ const userState = atom<User>({
   default: null,
 })
 
+async function createUserIfNotFound(user: User) {
+  const userRef = firebase.firestore().collection('users').doc(user.uid)
+  const doc = await userRef.get()
+  if (doc.exists) {
+    return
+  }
+  await userRef.set({
+    name: 'taro' + new Date().getTime(),
+  })
+}
+
 export function useAuthentication() {
   const [user, setUser] = useRecoilState(userState)
 
@@ -29,10 +40,12 @@ export function useAuthentication() {
     firebase.auth().onAuthStateChanged(function (firebaseUser) {
       if (firebaseUser) {
         console.log('Set user')
-        setUser({
+        const loginUser: User = {
           uid: firebaseUser.uid,
-          isAnonymous: firebaseUser.isAnonymous,
-        })
+          isAnonymous: firebaseUser.isAnonymous
+        }
+        setUser(loginUser)
+        createUserIfNotFound(loginUser)
       } else {
         // User is signed out.
         setUser(null)
