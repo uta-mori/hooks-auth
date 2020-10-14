90c0abd95b7b705516943c5ec28f03890766eb87
uta-mori <mori_yuta@retail-ai.jp>
2020-10-14 22:34:24 +0900

    ✨  uid変更時に再読み込み

--- pages/users/[uid].tsx
+++ pages/users/[uid].tsx
@@ -1,35 +1,35 @@
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { User } from '../../models/User'
-import * as firebase from 'firebase/app'
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { User } from "../../models/User";
+import * as firebase from "firebase/app";
 
 export default function UserShow() {
-  const [user, setUser] = useState<User>(null)
-  const router = useRouter()
-  const query = router.query as Query
+  const [user, setUser] = useState<User>(null);
+  const router = useRouter();
+  const query = router.query as Query;
 
-  useEffect(()=>{
+  useEffect(() => {
     if (query.uid === undefined) {
-      return
+      return;
     }
-    async function loadUser(){
+    async function loadUser() {
       const doc = await firebase
         .firestore()
-        .collection('users')
+        .collection("users")
         .doc(query.uid)
-        .get()
-      if (!doc.exists){
-        return
+        .get();
+      if (!doc.exists) {
+        return;
       }
-      const gotUser = doc.data() as User
-      gotUser.uid = doc.id
-      setUser(gotUser)
+      const gotUser = doc.data() as User;
+      gotUser.uid = doc.id;
+      setUser(gotUser);
     }
-    loadUser()
-  },[])
-  return <div>{user ? user.name : 'ロード中…'}</div>
+    loadUser();
+  }, [query.uid]);
+  return <div>{user ? user.name : "ロード中…"}</div>;
 }
 
 type Query = {
-  uid: string
-}
+  uid: string;
+};
